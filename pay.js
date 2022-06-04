aea00dfaa61b3f34afb56d2b4710d792b43a3a3d
HemsagarNM <91878791+HemsagarNM@users.noreply.github.com>
2022-06-04 17:38:33 +0530

    Add files via upload

---
+++ pay.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { PaytmButton } from "./Hackman/paytmButton";
+ 
+function App() {
+ return (
+   < div >
+     < PaytmButton / >
+   < /div >
+ );
+}
+ 
+export default App;
